9992ea42331c0160b545b6e2b5d695d340c96a43
leffty <brigante2004@yandex.ru>
2025-02-17 20:25:24 +0300

    v2.2

--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -1,21 +1,41 @@
 from rest_framework import serializers
 
+
 from .models import Post, Comment, Group
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = '__all__'
+        fields = (
+            'title',
+            'slug',
+            'description',
+            'post',
+            '',
+        )
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+
+    class Meta:
+        model = Post
+        fields = (
+            'pub_date',
+            'image',
+            'author',
+            'text',
+            'group',
+        )
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = (
+            'author',
+            'post',
+            'text',
+            'created',
+        )
